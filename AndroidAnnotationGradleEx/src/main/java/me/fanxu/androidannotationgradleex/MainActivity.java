8150be577d1023dfa7ab98adcbc65bf8de5310b0
FX <fanxu.china@gmail.com>
2013-08-06 23:08:40 +0800

    init

---
+++ AndroidAnnotationGradleEx/src/main/java/me/fanxu/androidannotationgradleex/MainActivity.java
@@ -0,0 +1,13 @@
+package me.fanxu.androidannotationgradleex;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+import com.googlecode.androidannotations.annotations.EActivity;
+
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+
+}
